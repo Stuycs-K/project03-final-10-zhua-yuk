69070621dbacd106f238a4f0c0588eed8311fe21
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2025-01-07 01:33:44 -0500

    header for subprocess

--- include/subprocess.h
+++ include/subprocess.h
@@ -1,7 +1,24 @@
 #ifndef SUBPROCESS_H
 #define SUBPROCESS_H
-#include "config.h"
+#include "types.h"
+/*
+spawn_subprocess
+    forks a subprocess that performs FDM calculations on a specific range
+    of data in shared memory. 
 
-int spawn_subprocess(struct point3i size, int ub, int lb, int order, int mode);
+ARGS
+    vec3i size - the size of the grid (in cells)
+    int ub - lower bound layer index
+    int lb - upper bound layer index
+    int order - what order to approach the array from (to minimize race conditions)
+        0 -> from the start (0, 0, 0)
+        1 -> from the end (i, j, k)
+    int mode - which shared memory array is being written to
+
+RETURN VALUE
+    -1 (fail) something went wrong
+    PID of subprocess
+*/
+int spawn_subprocess(vec3i size, int ub, int lb, int order, int mode);
 
 #endif
--- src/subprocess.c
+++ src/subprocess.c
@@ -1,1 +1,7 @@
+#include "memory.h"
+#include "fdmcalc.h"
 #include "subprocess.h"
+
+int spawn_subprocess(vec3i size, int ub, int lb, int order, int mode) {
+    return 1;
+}
