01a64df52455af0bfc1d1ae1a551de6145d98940
Ashley <azhu50@stuy.edu>
2025-01-08 15:19:43 -0500

    adjusted update_layer for changes from 2D arrays to 1D

--- src/fdmcalc.c
+++ src/fdmcalc.c
@@ -32,23 +32,21 @@ void * update_row(double * original, double * plusy, double * minusy, double * p
     }
 }
 
-void ** update_layer(double ** layer, double ** above, double ** below, int i_max, int j_max, double timestep, double units, double ** coeffs, double ** newlayer, int mode){
+void update_layer(double * layer, double * above, double * below, int i_max, int j_max, double timestep, double units, double * coeffs, double * newlayer, int mode){
     double * allzerorow = (double *) calloc(i_max, sizeof(double));
-    double * allzerocol = (double *) calloc(j_max, sizeof(double));
     for(int row = 0; row < j_max; row++){
-        int i = j_max-1-row;
+        int i = j_max-1-row; // row reading
         if(mode ==0){
-            i=row;
+            i=row; 
         }
-        double * currrow = newlayer[i];
         double * plusy = allzerorow;
         double * minusy = allzerorow;
-        ifg(row!=0){
-            plusy = layer[i-1];
+        if(row!=0){
+            plusy = layer[i_max*(i-1)];
         }
         if(row!=j_max-1){
-            minusy=layer[i+1];
+            minusy=layer[i_max*(i+1)];
         }
-        update_row(layer[i], plusy, minusy, above[i], below[i], coeffs[i], i_max, timestep, units, newlayer[i]);
+        update_row(layer[i_max*i], plusy, minusy, above[i_max*i], below[i_max*i], coeffs[i_max*i], i_max, timestep, units, newlayer[i_max*i]);
     }
 }
