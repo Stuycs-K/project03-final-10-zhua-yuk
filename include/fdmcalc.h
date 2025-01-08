b1cf5517bad74da2afa67d6a510655d4a5f9789c
Ashley <azhu50@stuy.edu>
2025-01-08 15:05:42 -0500

    made 2D arrays 1D

--- include/fdmcalc.h
+++ include/fdmcalc.h
@@ -41,25 +41,26 @@ RETURN VALUE
     void
 */
 void update_row(double * original, double * plusy, double * minusy, double * plusz, double * minusz, double * coeffs, int i_max, double timestep, double units, double * newrow);
+
 /*
 update_layer
     updates newlayer with the new temperatures for a layer given the temperatures of the layers directly above and below, coefficients, timestep, and units
 
 ARGS
-    double ** layer - 2D array of layer's temperatures
-    double ** above - 2D array of layer directly above's temperatures
-    double ** below - 2D array of layer directly below's temperatures
+    double * layer - array of layer's temperatures, going from left to right from the top row to the bottom row
+    double * above - array of layer directly above's temperatures, going from left to right from the top row to the bottom row
+    double * below - array of layer directly below's temperatures, going from left to right from the top row to the bottom row
     int i_max - number of columns in a layer
     int j_max - number of rows in a layer
     double timestep - timestep
     double units - unit per cell length
-    double ** coeffs - 2D array of thermal diffusivity coefficients associated with each cell in the layer
-    double ** newlayer - 2D array of layer with updated temperatures
+    double * coeffs - array of thermal diffusivity coefficients associated with each cell in the layer, going from left to right from the top row to the bottom row
+    double * newlayer - array of layer with updated temperatures, going from left to right from the top row to the bottom row
     int mode - will edit from row 0 to last row if mode is 0 and will edit in reverse order (from last row to row 0) otherwise
 Note: if there is no layer above/below the given row, double ** above or double ** below are double ** with appropriate dimensions full of 0s
 
 RETURN VALUE
     void
 */
-void update_layer(double ** layer, double ** above, double ** below, int i_max, int j_max, double timestep, double units, double ** coeffs, double ** newlayer, int mode);
+void update_layer(double * layer, double * above, double * below, int i_max, int j_max, double timestep, double units, double * coeffs, double * newlayer, int mode);
 #endif
