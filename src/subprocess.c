6617461a4980100d84f2915a83374e462a80fe55
Ashley <azhu50@stuy.edu>
2025-01-09 15:31:33 -0500

    down semaphore

--- src/subprocess.c
+++ src/subprocess.c
@@ -2,7 +2,25 @@
 #include "fdmcalc.h"
 #include "config.h"
 #include "subprocess.h"
+#include <unistd.h>
 
 int spawn_subprocess(vec3i size, int ub, int lb, int order, int mode) {
-    return 1;
+    int writeTo=4807;
+    int readFrom=11707;
+    if(mode ==0){
+        writeTo=11707;
+        readFrom=4807;
+    }
+    int f = fork();
+    if(f==0){
+        int sem_des = (1535, 1, 0);
+        struct mysembuf; 
+        mysembuf.sem_op = -1;
+        mysembuf.sem_num = 0; 
+        semop(sem_des, mysembuf, 1);
+        
+    }
+    else{
+        return f;
+    }
 }
