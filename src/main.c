5d4c01b72e3fbb5af815caf66b671491c927ee31
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2025-01-11 15:01:45 -0500

    fixed remove shared memory method

--- src/main.c
+++ src/main.c
@@ -6,5 +6,9 @@
 
 
 int main() {
-
+    grid_dimen dimen = read_fdata("test.csv", "out.csv");
+    write_data("out.csv", dimen.size, 0);
+    write_data("out.csv", dimen.size, 0);
+    write_data("out.csv", dimen.size, 0);
+    remove_shared_mem();
 }
