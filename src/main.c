91707622c6c6ae94bc572f16d0499d77cdd86439
Ashley <azhu50@stuy.edu>
2025-01-14 15:35:04 -0500

    reading semaphore

--- src/main.c
+++ src/main.c
@@ -13,9 +13,17 @@
 
 grid_dimen DIMENSIONS;
 int START, NEND, ORDER;
+union semun {
+  int val;                  
+  struct semid_ds *buf;     
+  unsigned short  *array;   
+  struct seminfo  *__buf;};
 
 int main() {
     DIMENSIONS = read_fdata("test.csv", "out.csv");
+    union semun semDATA;
+    int semVal = semctl(semget(SEMKEY, 0, 0), 0, GETVAL, semDATA);
+
     spawn_subprocess(0, DIMENSIONS.size.i, 0);
     sleep(1);
     printf("%d\n",write_data("out.csv", DIMENSIONS.size, 1) );
