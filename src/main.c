07f6fa48805b7a2ba25a2459f8f211647eeb279b
Ashley <azhu50@stuy.edu>
2025-01-14 22:01:47 -0500

    spawning subprocesses in main

--- src/main.c
+++ src/main.c
@@ -23,8 +23,26 @@ int main() {
     DIMENSIONS = read_fdata("test.csv", "out.csv");
     union semun semDATA;
     int semVal = semctl(semget(SEMKEY, 0, 0), 0, GETVAL, semDATA);
-
-    spawn_subprocess(0, DIMENSIONS.size.i, 0);
+    int num_SP = ceil(DIMENSIONS.size.z/LAYERS_PER_SP); 
+    int num_timesteps = (DIMENSIONS.tf)/DIMENSIONS.dt;
+    int timesteps_done = 0;
+    int * subprocessPIDs = malloc(sizeof(int)*)
+    int order = 0;
+    int parentPID = getpid();
+    for(int layers_done = 0; layers_done <DIMENSIONS.z; layers_done+=LAYERS_PER_SP){
+      int subPID = 0; 
+      if(layers_done+LAYERS_PER_SP>DIMENSIONS.z+1){
+        subPID = spawn_subprocess(layers_done,DIMENSIONS.z+1,order%2);
+      }
+      else{
+        subPID = spawn_subprocess(layers_done,layers_done+LAYERS_PER_SP,order%2);
+      }
+      if(subPID>0){
+        subprocessPIDs[order] = subPID;
+      }
+      order++;
+    }
     sleep(1);
+    printf("myPID: %d\n", getpid());
     printf("%d\n",write_data("out.csv", DIMENSIONS.size, 1) );
 }
